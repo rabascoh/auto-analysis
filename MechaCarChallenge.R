6e7200395402dcf155a70bd954897838cb42331a
rabascoh <80366530+rabascoh@users.noreply.github.com>
2021-09-26 16:15:59 -0400

    Deliverable 2 update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,7 +1,26 @@
+#Deliverable 1
+
+#Load dplyr package
 library(dplyr)
 
+#Read in MechaCar mpg data
 mpg <- read.csv(file="MechaCar_mpg.csv")
 
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg) #generate multiple linear regression model
+#Perform multiple linear regression
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg) 
 
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg)) #generate summary statistics
+#Determine p-value and r-squared value
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg))
+
+#Deliverable 2
+
+#Read in Suspension Coil data
+coil <- read.csv(file="Suspension_Coil.csv")
+
+#Create total summary table
+total_summary <- coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+View(total_summary)
+
+#Create summary table by lot
+lot_summary <- coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+View(lot_summary)
